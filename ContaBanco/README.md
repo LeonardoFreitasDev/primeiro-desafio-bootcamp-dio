a8ebdbd23f16ee9c5f2537b5460d9af4ed5e339f
LeonardoFreitasDev <leonardo.lfs18@gmail.com>
2024-04-26 08:57:39 -0300

    Resolução do desafio proposto

---
+++ ContaBanco/README.md
@@ -0,0 +1,9 @@
+## Segundo Desafio Bootcamp Java para Backend DIO.me
+
+O códido java do repositório contém a resolução do problema proposto na aula.
+
+## Problema proposto
+
+O desafio problema proposto na aula está disponível no link abaixo:
+
+https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // Atributos da classe
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner input = new Scanner(System.in); // Instancia a classe Scanner para caapturar as entradas no terminal
+        input.useLocale(Locale.US); // Define o tipo de input com base no padrão do local
+
+        System.out.println("Digite o número da sua conta: ");
+        numero = input.nextInt();
+        input.nextLine(); // Limpeza do terminal para não capturar a quebra de linha no próximo input
+
+        System.out.println("Digite o número da sua agência: ");
+        agencia = input.nextLine();
+
+        System.out.println("Digite o seu nome completo: ");
+        nomeCliente = input.nextLine();
+
+        System.out.println("Digite o seu saldo: ");
+        saldo = input.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        input.close(); // Fechamennto da instancia do Scanner
+    }
+}
